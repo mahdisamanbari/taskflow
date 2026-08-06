5a5645eb0f7d8a79ca6fd20381b2fb35231a0aad
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2026-08-06 16:54:17 +0330

    feat: add colored task status badges

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,19 @@
 import styles from '../components/TaskCard.module.css'
 function TaskCard({ title, status }) {
+    const statusClass = {
+        Todo: styles.todo,
+        "In Progress": styles.inProgress,
+        Done: styles.done,
+      };
     return (
-    <article className={styles.card}>
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.status}>{status}</p>
-    </article>
+        <article className={styles.card}>
+        <h3 className={styles.title}>{title}</h3>
+  
+        <span className={`${styles.status} ${statusClass[status]}`}>
+            
+          {status}
+        </span>
+      </article>
     );
   }
   
