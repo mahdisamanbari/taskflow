1f520a09b4edd0904451f6d9094abf3b7d399cb2
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2026-08-06 15:36:10 +0330

    feat: add sidebar icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import styles from "./Sidebar.module.css";
+import { MdDashboard } from "react-icons/md";
+import { FaTasks } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <h2 className={styles.logo}>TaskFlow</h2>
 
       <ul className={styles.menu}>
-        <li>Dashboard</li>
-        <li>Tasks</li>
-        <li>Settings</li>
-      </ul>
+  <li>
+    <MdDashboard />
+    <span>Dashboard</span>
+  </li>
+
+  <li>
+    <FaTasks />
+    <span>Tasks</span>
+  </li>
+
+  <li>
+    <IoSettingsSharp />
+    <span>Settings</span>
+  </li>
+</ul>
     </aside>
   )
 }
